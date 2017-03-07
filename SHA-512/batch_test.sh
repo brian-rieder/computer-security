cc3f6de882da8c935fe5535b322e2ef8dfaffc0b
Brian Rieder <brieder@purdue.edu>
2017-03-07 11:04:21 -0500

    Hash testing script using random input from fortune

---
+++ SHA-512/batch_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Testing hash function..."
+successes=0
+testcases=10
+
+for i in $(seq 1 $testcases) ; do
+  echo "=================================================="
+  echo "Test Case $i: "
+  echo ""
+  fortune | tee tmp.txt
+  echo ""
+  output=$(hw07.py tmp.txt)
+  expected=$(echo "Hash matches 'hashlib' hex digest!")
+  diff <(echo $output) <(echo $expected)
+  if [ $? -eq 0 ] ; then
+    printf "\033[0;32mPASSED\n\033[0m"
+    (( successes += 1 ))
+  else
+    printf "\033[0;31mFAILED\n\033[0m"
+  fi
+done
+echo "=================================================="
+rm tmp.txt
+echo "Results: $successes / $testcases"
